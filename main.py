1535fe605155fe81fa5b438ac1eb3cdaf34c1c9f
Aaditya Awati <107744138+AadityaAwati@users.noreply.github.com>
2025-04-05 15:17:54 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+import tkinter
+
+main_window = tkinter.Tk()
+
+main_window.configure(bg='')
+main_window.wm_attributes('-fullscreen', True)
+
+main_window.title("Virtual Input Nullifier")
+
+main_window.mainloop()
