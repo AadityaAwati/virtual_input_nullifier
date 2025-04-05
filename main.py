595139cb7306f4b1ba0719204cea3724b17ae282
Aaditya Awati <107744138+AadityaAwati@users.noreply.github.com>
2025-04-05 22:08:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import tkinter
 
-main_window = tkinter.Tk()
+def run_nullifier():
+    main_window = tkinter.Tk()
 
-main_window.configure(bg='')
-main_window.wm_attributes('-fullscreen', True)
+    main_window.configure(bg='')
+    main_window.wm_attributes('-fullscreen', True)
 
-main_window.title("Virtual Input Nullifier")
+    main_window.title("Virtual Input Nullifier")
+    main_window.iconbitmap("danger_skull.ico")
 
-main_window.mainloop()
+    main_window.mainloop()
+
+run_nullifier()
